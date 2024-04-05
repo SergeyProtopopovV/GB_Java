d94f6bffec58ff207b9eeb4ad456dd6c0c408ab1
Sergey Protopopov <protopopov_s@mail.ru>
2024-04-05 22:48:04 +0300

    seminar 01

---
+++ seminar01/src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] array = new int[][] {
+                {2, 3},
+                {1, 2, 3 ,4}
+        };
+
+        String toReverse = "Добро пожаловать на курс по Java";
+
+        System.out.println(Helper.reverse(toReverse));
+//        int[] arr = new int[]{1,2,4,76,89,5,-100};
+//        System.out.println(Helper.getSumElemsFromArray(array));
+    }
+}
