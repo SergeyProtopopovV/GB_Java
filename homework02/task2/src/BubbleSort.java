dbed455a74355febc71ad102152149bb7ead9a33
Sergey Protopopov <protopopov_s@mail.ru>
2024-04-10 22:59:01 +0300

    homework 02 started

---
+++ homework02/task2/src/BubbleSort.java
@@ -0,0 +1,51 @@
+//Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
+//
+//Напишите свой код в методе sort класса BubbleSort. Метод sort принимает на вход один параметр:
+//
+//int[] arr - числовой массив
+//После каждого прохода по массиву ваш код должен делать запись в лог-файл 'log.txt'
+//в формате год-месяц-день час:минуты {массив на данной итерации}.
+//Для логирования использовать логгер logger класса BubbleSort.
+//        Пример
+//        arr = new int[]{9, 4, 8, 3, 1};
+//sort(arr)
+//
+//// При чтении лог-файла получим:
+// 2023-05-19 07:53 [4, 8, 3, 1, 9]
+// 2023-05-19 07:53 [4, 3, 1, 8, 9]
+// 2023-05-19 07:53 [3, 1, 4, 8, 9]
+// 2023-05-19 07:53 [1, 3, 4, 8, 9]
+// 2023-05-19 07:53 [1, 3, 4, 8, 9]
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+class BubbleSort {
+    private static File log;
+    private static FileWriter fileWriter;
+
+    public static void sort(int[] mas) {
+
+        boolean isSorted = false;
+        int buf;
+        while(!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < mas.length-1; i++) {
+                if(mas[i] > mas[i+1]){
+                    isSorted = false;
+
+                    buf = mas[i];
+                    mas[i] = mas[i+1];
+                    mas[i+1] = buf;
+                }
+            }
+            System.out.println(Arrays.toString(mas));
+        }
+    }
+}
