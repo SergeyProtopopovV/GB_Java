b1b7b4b5ce9726e3d37716fd27d3a2915a77cfb3
Sergey Protopopov <protopopov_s@mail.ru>
2024-04-23 22:48:56 +0300

    lesson06

--- lection06/src/Lesson_06/part2/Ex001_Program.java
+++ lection06/src/Lesson_06/part2/Ex001_Program.java
@@ -7,25 +7,25 @@ public class Ex001_Program {
     public static void main(String[] args) {
         // #region
 
-        Worker w1 = new Worker();
+        Lesson_06.part2.Worker w1 = new Lesson_06.part2.Worker();
         w1.firstName = "Имя_1";
         w1.lastName = "Фамилия_1";
         w1.salary = 100;
         w1.id = 1000;
 
-        Worker w4 = new Worker();
+        Lesson_06.part2.Worker w4 = new Lesson_06.part2.Worker();
         w4.firstName = "Имя_1";
         w4.lastName = "Фамилия_1";
         w4.salary = 100;
         w4.id = 1000;
 
-        Worker w2 = new Worker();
+        Lesson_06.part2.Worker w2 = new Lesson_06.part2.Worker();
         w2.firstName = "Имя_2";
         w2.lastName = "Фамилия_2";
         w2.salary = 200;
         w2.id = 2000;
 
-        Worker w3 = new Worker();
+        Lesson_06.part2.Worker w3 = new Lesson_06.part2.Worker();
         w3.firstName = "Имя_3";
         w3.lastName = "Фамилия_3";
         w3.salary = 300;
@@ -38,7 +38,7 @@ public class Ex001_Program {
 
         System.out.println(w1==w4);
         System.out.println(w1.equals(w4));
-        var workers = new HashSet<Worker>(Arrays.asList(w1, w2, w3));
+        var workers = new HashSet<Lesson_06.part2.Worker>(Arrays.asList(w1, w2, w3));
         System.out.println(workers.contains(w4));
         
     }
